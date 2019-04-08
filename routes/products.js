d753aadf42a56c467af4e1ac4ada6c958d5ef1d9
contrerasjf <contrerasjf.0@hotmail.com>
2019-04-08 01:06:06 -0500

    feat(Products): Implement the service layer
    * The body-parse library was installed and it was added like a middleware.
    * The new services was implemented in the Api and view interfaces.
    * The Products services file was created.
    
    
    Resolve: C12

--- routes/products.js
+++ routes/products.js
@@ -2,8 +2,19 @@ const express = require("express");
 const router = express.Router();
 const productMocks = require('../utils/mocks/products');
 
-router.get("/", function(req, res) {
-  res.render("products", { products });
+const productService = new ProductsService();
+
+router.get("/", function(req, res, next ) {
+  
+  const { tags } = req.query;
+  try {
+    const products = await productService.getProducts({ tags });
+    res.render("products", { products });
+  } catch (err) {
+    next(err);
+  }
+
+
 });
 
 
