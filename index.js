9542b9ed1682c8600530d509dfe5674d29c12356
contrerasjf <contrerasjf.0@hotmail.com>
2019-04-08 07:57:10 -0500

    refactor(files): ordering and adjusting our project
    Resolve: C13

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require('body-parser');
+const productsRouter = require('./routes/views/products');
+const productsApiRouter = require('./routes/views/products');
+
+// app
 const app = express();
-const productsRouter = require('./routes/products');
-const productsApiRouter = require('./routes/api/products');
-const bodyParse = require('body-parse');
 
+// middlewares
+app.use(bodyParser.json());
 
+// static files
 app.use("/static", express.static(path.join(__dirname, "public")));
 
+// view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+
+// routes
 app.use("/products", productsRouter);
 app.use("/api/products", productsApiRouter);
 
+// redirect
+app.get('/', function(req, res) {
+  res.redirect('/products');
+});
 
-app.use(bodyParse.json());
-
+// server
 const server = app.listen(8000, function() {
   console.log(`Listening http://localhost:${server.address().port}`);
 });
